fea4435cdec6c28769a2a7d6f4d71340141aced9
Oliver Bartlett <bartlett@robots.ox.ac.uk>
2016-10-21 13:57:06 +0100

    Updated to include RMS

--- root.m
+++ root.m
@@ -15,12 +15,12 @@ if strcmp(data,'Sotonmet')
     y = Tideheight_raw; y(isnan(Tideheight_raw)) = []; % Similarly for Tide Heights vector
     y_t = TrueTideHeight_raw;
     x_t = time_raw;
-    x_s = linspace(time_raw(1), time_raw(end),500)'; %Discretises grid to calculate GP
+    x_s = time_raw; %linspace(time_raw(1), time_raw(end),500)'; %Discretises grid to calculate GP
     
 else
     x = sort(randn(20, 1));                 % 20 training inputs
     y = sin(3*x) + 0.1*randn(length(x),1);  % 20 noisy training targets
-    y_t = sin(3*x);
+    y_t = sin(3*x); x_t = x;
     x_s = linspace(-3, 3, 61)';                  % 61 test inputs
     
     start_time = 0;
@@ -42,11 +42,21 @@ end
 
 %% Define GP and Hyperparameters
 
-meanfunc = [];                    % empty: don't use a mean function
-covfunc = @covSEiso;              % Squared Exponental covariance function
+meanfunc = @meanConst;                    % empty: don't use a mean function
+covfunc = @covSEiso; % Squared Exponental covariance function
 likfunc = @likGauss;              % Gaussian likelihood
 
-hyp = struct('mean', [], 'cov', [0 0], 'lik', -1);
+hyp = struct('mean', 3, 'lik', -1);
+
+hyp.cov = [0 10];              
+
+%c1 = {@covPeriodic}; cov1 = [0 0 0];
+%c2 = {@covSEiso}; cov2 = [0 10];
+%hyp.cov = [cov1 cov2], 
+
+%covfunc = {'covProd',{c1, c2}};
+
+
 
 %% Calculate and Plot Unoptimised GP
 
@@ -60,6 +70,8 @@ hold on
 plot(x_t+start_time,y_t,'bx') %Plot true data
 datetick('x','keeplimits')
 hold off
+
+disp(strcat('Unoptimised RMS Error: ',num2str(rms(mu-y_t))))
 
 clear mu s2
 %% Calculate and Plot Optimised GP
@@ -75,3 +87,7 @@ hold on
 plot(x_t+start_time,y_t,'bx') % Plot true data
 datetick('x','keeplimits')
 hold off
+disp(strcat('Optimised RMS Error: ',num2str(rms(mu-y_t))))
+%% RMSE
+
+%a = rms(mu-y_t);
